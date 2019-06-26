1f6a812c26d029a99ae50ede3c08ff0385183edb
lena <lerlach@student.ethz.ch>
2019-06-26 11:08:54 +0200

    Calculation of the distance measures;

--- GP_implementation/gen_test_seq_slice_try.py
+++ GP_implementation/gen_test_seq_slice_try.py
@@ -20,7 +20,7 @@ input_train_KD = input_dir + 'HC_KDvals.csv'
 
 
 ## SET OUTPUT DIRECTORIES (for plots to save)
-dir_out = '/media/lena/LENOVO/Dokumente/Masterarbeit/data/GP/intermediate_testmut/'
+dir_out = '/media/lena/LENOVO/Dokumente/Masterarbeit/data/Plots/GP_model/gen_pred_seqs/'
 
 # If the output directories do not exist, then create it
 if not os.path.exists(dir_out):
@@ -88,135 +88,20 @@ print("possible sequences from these combination {0:.1f} * 10^13".format(num_seq
 
 
 
-################ CREATE NEW SEQUENCES USING ITERTOOLS #################
 
-# # store keys
-# positions = sorted(mut_dict)
-# # create object of combinations
-# combis = it.product(*(mut_dict[pos] for pos in positions))
-#
-# # get slice of the combinations
-# slice = it.islice(combis, 1000)
-#
-# sl_list = list(slice)
-# # new_seq = np.asarray([''.join(x) for x in sl_list])
+################################### PREDICT AFFINITIES #################################
 
-########################################################
+######### use LAZY CARTESIAN PRODUCT FOR RANDOM SAMPLING #######
 
-
-
-
-
-
-#### PREDICT AFFINITIES ####
-
-########################### PREDICT LD KERNEL #######################
-#### make a loop of testing the LD kernel
-# get optimal noise parameter
-# opt_param = GP.get_params(X_train, y_train, init_param=1)
-#
-# # predict the test set and training set
-# # mu_test, var_test = GP.predict_GP(X_train, y_train, X_test, opt_param)
-#
-# mu_all = []
-# var_all = []
-#
-# # store keys
-# positions = sorted(mut_dict)
-# # create object of combinations
-# combis = it.product(*(mut_dict[pos] for pos in positions))
-#
-#
-# for i in range(0, 50000, 500):
-#
-#     slice = it.islice(combis, i, i + 500)
-#
-#     sl_list = list(slice)
-#     X_test = np.asarray([''.join(x) for x in sl_list])
-#
-#
-#     mu_test, var_test = GP.predict_GP(X_train, y_train, X_test, opt_param)
-#     mu_all.append(mu_test)
-#     var_all.append(var_test)
-#     print("samples processed: {}".format(i+500))
-#     # print("cycles left: {}".format((10000 - i + 500)/500)
-#
-# # save intermediate mus
-#
-#
-# df_muvar = pd.DataFrame()
-# df_muvar['mu'] = np.concatenate(mu_all)
-# df_muvar['var'] = np.concatenate(var_all)
-#
-# df_muvar.to_csv(dir_outLD + 'intermed_muvars.csv', sep=',')
-
-
-###########################################################################################
-
-
-
-
-########################### PREDICT MATERN KERNEL #######################
-#### make a loop of testing the Matern kernel
-
-#### TEST TEST SET ####
-
-# get optimal noise parameter
-
-# X_train_OH = GP.one_hot_encode_matern(X_train)
-# opt_param = GP.get_params_mat(X_train_OH, y_train)
-# # opt_param = [0.1,10]
-#
-#
-# mu_all_mat = []
-# var_all_mat = []
-# steps = 1000
-#
-#
-# # store keys
-# positions = sorted(mut_dict)
-# # create object of combinations
-# combis = it.product(*(mut_dict[pos] for pos in positions))
-
-
-
-####################### SLICE POSSIBLE SEQUENCES WITH ITERTOOLS ######################
-
-# for i in range(0, 1000000, steps):
-#
-#     slice = it.islice(combis, i, i + steps)
-#
-#     sl_list = list(slice)
-#     X_test = np.asarray([''.join(x) for x in sl_list])
-#     X_test_OH = GP.one_hot_encode_matern(X_test)
-#
-#     mu_test, var_test = GP.predict_GP_mat(X_train_OH, y_train, X_test_OH, opt_param)
-#     mu_all_mat.append(mu_test)
-#     var_all_mat.append(var_test)
-#     print("samples processed: {}".format(i+steps))
-#     # print("cycles left: {}".format((10000 - i + 500)/500)
-#
-# # save intermediate mus
-#
-# df_muvar = pd.DataFrame()
-# df_muvar['mu'] = np.concatenate(mu_all_mat)
-# df_muvar['var'] = np.concatenate(var_all_mat)
-#
-# df_muvar.to_csv(dir_out + 'intermed_muvars10_6.csv', sep=',')
-#
-#
-
-
-####################### LAZY CARTESIAN PRODUCT ######################
 # from: https://gist.github.com/iamtheburd/78f816b1b0082956710107671c2ec83e#file-lazy-cartesian-product-py
 # explained:  https://hackernoon.com/generating-the-nth-cartesian-product-e48db41bed3f
 
 
 # number of sequences to generate randomly
-num_gen = 100000
+num_gen = 10000
 
 # specify number of sequences that should be processed once
-proc_step = 10000
+proc_step = 1000
 
 
 sets = [mut_dict[x] for x in mut_dict]
@@ -236,8 +121,18 @@ opt_param = GP.get_params_mat(X_train_OH, y_train)
 # opt_param = [0.1,10]
 
 
+###### PREDICT RANDOM SAMPLE AT ONCE USING LAZY CARTESIAN PRODUCT ######################
 
-####################### SLICE LAZY CARTESIAN PRODUCT ######################
+
+# X_test = np.asarray([''.join(cp.entryAt(x)) for x in random_idx])
+# X_test_OH = GP.one_hot_encode_matern(X_test)
+#
+# # predict X_test
+# mu_test, var_test = GP.predict_GP_mat(X_train_OH, y_train, X_test_OH, opt_param)
+
+
+
+###### SLICE LAZY CARTESIAN PRODUCT ######################
 
 # write slice wise in file
 
@@ -277,9 +172,8 @@ with open(file_name, 'w') as f_out:
 
 
 
-################################## to do!!!!! ######################
 
-####################### GENERATE ALL POSSIBLE SEUQENCES ######################
+####################### GENERATE ALL POSSIBLE SEUQENCES with threshold ######################
 
 # specify number of sequences that should be processed once
 proc_step = 1000
@@ -361,7 +255,135 @@ axs[1].title.set_text('Distribution of true KDs')
 
 # axs[0].set_xlim([-3, 3])
 # axs[1].set_xlim([-3, 3])
+
+plt.savefig(str(dir_out+'histogram_pred_seqs.png'))
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################## ITERTOOLS (NOT USED) #######################
+
+################ CREATE NEW SEQUENCES USING ITERTOOLS #################
+
+# # store keys
+# positions = sorted(mut_dict)
+# # create object of combinations
+# combis = it.product(*(mut_dict[pos] for pos in positions))
+#
+# # get slice of the combinations
+# slice = it.islice(combis, 1000)
+#
+# sl_list = list(slice)
+# # new_seq = np.asarray([''.join(x) for x in sl_list])
+
+########################################################
+
+########################### PREDICT LD KERNEL #######################
+#### make a loop of testing the LD kernel
+# get optimal noise parameter
+# opt_param = GP.get_params(X_train, y_train, init_param=1)
+#
+# # predict the test set and training set
+# # mu_test, var_test = GP.predict_GP(X_train, y_train, X_test, opt_param)
+#
+# mu_all = []
+# var_all = []
+#
+# # store keys
+# positions = sorted(mut_dict)
+# # create object of combinations
+# combis = it.product(*(mut_dict[pos] for pos in positions))
+#
+#
+# for i in range(0, 50000, 500):
+#
+#     slice = it.islice(combis, i, i + 500)
+#
+#     sl_list = list(slice)
+#     X_test = np.asarray([''.join(x) for x in sl_list])
+#
+#
+#     mu_test, var_test = GP.predict_GP(X_train, y_train, X_test, opt_param)
+#     mu_all.append(mu_test)
+#     var_all.append(var_test)
+#     print("samples processed: {}".format(i+500))
+#     # print("cycles left: {}".format((10000 - i + 500)/500)
+#
+# # save intermediate mus
+#
+#
+# df_muvar = pd.DataFrame()
+# df_muvar['mu'] = np.concatenate(mu_all)
+# df_muvar['var'] = np.concatenate(var_all)
+#
+# df_muvar.to_csv(dir_outLD + 'intermed_muvars.csv', sep=',')
+
+
+###########################################################################################
+
+
+########################### PREDICT MATERN KERNEL #######################
+
+#### TEST WHOLE SET ####
+
+# get optimal noise parameter
+
+# X_train_OH = GP.one_hot_encode_matern(X_train)
+# opt_param = GP.get_params_mat(X_train_OH, y_train)
+# # opt_param = [0.1,10]
+#
+#
+# mu_all_mat = []
+# var_all_mat = []
+# steps = 1000
+#
+#
+# # store keys
+# positions = sorted(mut_dict)
+# # create object of combinations
+# combis = it.product(*(mut_dict[pos] for pos in positions))
+
+
+
+####################### SLICE POSSIBLE SEQUENCES WITH ITERTOOLS MATERN KERNEL ######################
+
+# for i in range(0, 1000000, steps):
+#
+#     slice = it.islice(combis, i, i + steps)
+#
+#     sl_list = list(slice)
+#     X_test = np.asarray([''.join(x) for x in sl_list])
+#     X_test_OH = GP.one_hot_encode_matern(X_test)
+#
+#     mu_test, var_test = GP.predict_GP_mat(X_train_OH, y_train, X_test_OH, opt_param)
+#     mu_all_mat.append(mu_test)
+#     var_all_mat.append(var_test)
+#     print("samples processed: {}".format(i+steps))
+#     # print("cycles left: {}".format((10000 - i + 500)/500)
+#
+# # save intermediate mus
+#
+# df_muvar = pd.DataFrame()
+# df_muvar['mu'] = np.concatenate(mu_all_mat)
+# df_muvar['var'] = np.concatenate(var_all_mat)
+#
+# df_muvar.to_csv(dir_out + 'intermed_muvars10_6.csv', sep=',')
+#
+#
 
 
 
