70b09368fa9186244a0d3462622f22df4698e28c
lena <lerlach@student.ethz.ch>
2019-06-27 16:10:27 +0200

    Calculation of the distance measures;

--- GP_implementation/seqs_from_idx_lazy_cart_prod.py
+++ GP_implementation/seqs_from_idx_lazy_cart_prod.py
@@ -1,24 +1,30 @@
+'''
+Script to extract the indices of the random samples and generate the VDJ sequences
+with an AA dictionary of the possible combinations. Sort and get the top or lowest
+1000 sequences.
+'''
 
 
 import numpy as np
 import pandas as pd
-
 from GP_implementation import lazyCartProduct as catprod
 
 
 
 # Set input directories
 input_dir = '/media/lena/LENOVO/Dokumente/Masterarbeit/data/GP/input/'
-in_dir = '/media/lena/LENOVO/Dokumente/Masterarbeit/data/GP/gen_seqs_muvar/'
+in_dir = '/media/lena/LENOVO/Dokumente/Masterarbeit/data/GP/gen_seqs_muvar/midr/'
 in_dir_seq = input_dir + 'input_HCs.csv'
 
-out_dir = in_dir + 'gen_pred_seqs_thresh/output/'
+out_dir = in_dir
 
 
 # load the indeces
 
 new_seqs = pd.DataFrame()
-new_seqs = pd.read_csv(in_dir+'gen_pred_seqs_thresh/output/muvars_sliced_10_7.txt', delimiter='\t', skiprows=1)
+
+# !!!!! change 10_5 or 10_7 !!!!
+new_seqs = pd.read_csv(in_dir+'muvars_sliced_10_5.txt', delimiter='\t', skiprows=1)
 
 idx = list(new_seqs.random_idx.values)
 
@@ -61,11 +67,19 @@ sets = [mut_dict[x] for x in mut_dict]
 cp = catprod.LazyCartesianProduct(sets)
 
 
-# save new seqs
-
+# sort and save top 1000 new seqs
 new_seqs['Sequences'] = np.asarray([''.join(cp.entryAt(x)) for x in idx])
 
-new_seqs.to_csv(out_dir+'new_seq_gen_pos.csv')
+# acending = True for neg and midrange; False for positive
+new_seqs.sort_values(by='mus', inplace = True, ascending=True)
+
+# get 1000 sequences in the middle for midrange
+new_seqs = new_seqs.iloc[49188:50188, ]
+
+# get first 1000 ones for negative/positive
+# new_seqs = new_seqs.iloc[:1000, ]
+
+new_seqs.to_csv(out_dir+'new_seq_gen_mid.csv')
 
 
 
