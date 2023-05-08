d5216bba5c1781948945f0a7c458b95062e87cc0
lena16011 <lena.erlach@gmail.com>
2023-05-08 11:50:39 +0200

    cleaned up KD_box_violin_plot.py

--- Analysis_for_publication_R/KD_box_violin_plot.py
+++ Analysis_for_publication_R/KD_box_violin_plot.py
@@ -16,6 +16,71 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import ttest_ind
 
+###### BOX PLOT - FUNCTION ######
+def box_plot_KDs(data, x = None, y = None, groups=None, color_palette = 'Paired', fig_size = (6, 8), save_fig = None):
+    '''
+    Function to plot the KD values as a box plot with whiskers, no fliers
+    params:
+        data (pd.DataFrame()) : dataframe with the KD values and column as for the groups, if t-test should be performed;
+        x (string) : column name of the x values (groups column), e.g. "dataset", or None
+        y (string): column name of the y values (KD val column), e.g. "KD_nM"
+        groups (None, list of strings): if None, no t-test will be performed, else list of entries in the x (e.g. group) column of
+        the dataframe, comparison of these 2 groups in the t-test;
+        color_palette (list of strings, string): HEX values of the colors to be used in the plot or name of the palette (Matplotlib, Seaborn);
+        save_fig (None, os.Path) : if None, plot will only be shown, if os.Path is provided, the plot will be saved in the os.Path file name;
+
+    returns: None
+    '''
+    if groups != None:
+        # statistical test btw KD values
+        group1 = data[y][data[x] == groups[0]]
+        group2 = data[y][data[x] == groups[1]]
+        # Run the t-test
+        t = ttest_ind(group1, group2)
+        # The t-test returns 2 values: the test statistic and the pvalue
+        #print(np.round(t[1], 5))
+
+
+    # setup a box plot with the original sequences
+    sns.set(
+            style="ticks",                   # The 'ticks' style
+            rc={"figure.figsize": fig_size,      # width = 6, height = 9
+                })  # Axes colour
+
+    # custom palette
+    if len(color_palette) > 1:
+        c_pal = sns.color_palette(color_palette)
+    else:
+        c_pal = color_palette
+
+    b = sns.boxplot(data = data, x = x, y = y,
+                    width=0.6,  # The width of the boxes
+                    palette=c_pal,
+                    showfliers = False)  # Stop showing the fliers)
+    b = sns.stripplot(data = data, x = x, y = y,
+                      color = "black",)
+
+    # Set the y axis and font size
+    b.set_ylabel("kD [nM]", fontsize = 14)
+    # Set the x axis label and font size
+    b.set_xlabel("selected mAb variants", fontsize = 14)
+
+    if groups != None:
+        # Set the plot title and font size
+        plt.suptitle("kD values of selected & novel mAb variants", fontsize=16)
+        # subtitle
+        b.set_title("p-value = {}".format(np.round(t[1], 5)), fontsize = 14)
+    else:
+        # Set the plot title and font size
+        b.set_title("kD values of the selected mAb variants", fontsize=16)
+    # Remove axis spines
+    sns.despine(offset = 5, trim = True)
+
+    if save_fig != None:
+        plt.savefig(save_fig)
+    plt.show()
+
+
 ###### SET INPUT DIRECTORIES & LOAD DATA ######
 # Set paths to originally selected and novel sequences
 in_dir1 = '/Users/lerlach/Documents/current_work/GP_publication/code_git/Lena/GP_implementation/data/final_validation/'
@@ -39,109 +104,21 @@ KD_seqs_novel = pd.read_csv(in_f1, delimiter = ',')
 KD_seqs_novel.columns = ['SampleID', 'IDs', 'KD_M', 'KD_nM', 'label', 'VDJ_AA']
 KD_seqs_novel["dataset"] = "novel_selected"
 
-###### BOX PLOT - ORIGINAL SEQUENCES ######
-# setup a box plot with the original sequences
-sns.set(
-        style="ticks",                   # The 'ticks' style
-        rc={"figure.figsize": (6, 8),      # width = 6, height = 9
-            })  # Axes colour
-
-
-b = sns.boxplot(data = KD_seqs_orig, y = "KD_nM",
-                width=0.4,  # The width of the boxes
-                color='#1874cd',
-                #palette="Paired",
-                showfliers = False)  # Sop showing the fliers)
-b = sns.stripplot(data = KD_seqs_orig,
-                  y = "KD_nM",
-                  color = "black",)
-
-# Set the y axis and font size
-b.set_ylabel("kD [nM]", fontsize = 14)
-# Set the x axis label and font size
-b.set_xlabel("selected mAb variants", fontsize = 14)
-# Set the plot title and font size
-b.set_title("kD values of the selected mAb variants", fontsize = 16)
-# Remove axis spines
-sns.despine(offset = 5, trim = True)
-
-plt.show()
-
-
-
-
-###### BOX PLOT - NOVEL SEQUENCES ######
-sns.set(
-        style="ticks",                   # The 'ticks' style
-        rc={"figure.figsize": (6, 8),      # width = 6, height = 9
-            })  # Axes colour
-
-b = sns.boxplot(data = KD_seqs_novel, y = "KD_nM",
-                width=0.4,  # The width of the boxes
-                color='#cd0000',
-                #palette="Paired",
-                showfliers = False)  # Sop showing the fliers)
-b = sns.stripplot(data = KD_seqs_novel,
-                  y = "KD_nM",
-                  color = "black",)
-
-# Set the y axis and font size
-b.set_ylabel("kD [nM]", fontsize = 14)
-# Set the x axis label and font size
-b.set_xlabel("selected mAb variants", fontsize = 14)
-# Set the plot title and font size
-b.set_title("kD values of the selected mAb variants", fontsize = 16)
-# Remove axis spines
-sns.despine(offset = 5, trim = True)
-
-plt.show()
-
-
-
-###### BOX PLOT - COMBINED SEQUENCES ######
 # merge the dataframes
 KD_merge = pd.concat([KD_seqs_orig, KD_seqs_novel])
 KD_merge = KD_merge.dropna(subset=['KD_nM'])
 
-# statistical test btw KD values
-group1 = KD_merge['KD_nM'][KD_merge['dataset'] == "originally_selected"]
-group2 = KD_merge['KD_nM'][KD_merge['dataset'] == "novel_selected"]
-# Run the t-test
-t = ttest_ind(group1, group2)
-# The t-test returns 2 values: the test statistic and the pvalue
-print(np.round(t[1], 5))
 
+###### BOX PLOT - COMBINED SEQUENCES ######
+color_palette = ['#1874cd','#cd0000']
+box_plot_KDs(KD_merge, x = "dataset", y = "KD_nM", groups=["originally_selected", "novel_selected"],
+             color_palette = color_palette, save_fig = os.path.join(save_path, 'KD_originally_novel_variants_Ttest.pdf'))
 
-# setup a box plot with the original sequences
-sns.set(
-        style="ticks",                   # The 'ticks' style
-        rc={"figure.figsize": (6, 8),      # width = 6, height = 9
-            })  # Axes colour
+###### BOX PLOT - ORIGINAL SEQUENCES ######
+box_plot_KDs(KD_seqs_orig, x = None, y = "KD_nM", groups=None,
+             color_palette = ['#1874cd'], fig_size = (5,8), save_fig = os.path.join(save_path, 'KD_original_variants.pdf'))
 
-# custom palette
-c_pal = sns.color_palette(['#1874cd','#cd0000'])
-
-b = sns.boxplot(data = KD_merge, x = 'dataset', y = "KD_nM",
-                width=0.6,  # The width of the boxes
-                #color=['#1874cd','#cd0000'],
-                palette=c_pal,
-                showfliers = False)  # Sop showing the fliers)
-b = sns.stripplot(data = KD_merge, x = 'dataset',
-                  y = "KD_nM",
-                  color = "black",)
-
-# Set the y axis and font size
-b.set_ylabel("kD [nM]", fontsize = 14)
-# Set the x axis label and font size
-b.set_xlabel("selected mAb variants", fontsize = 14)
-# Set the plot title and font size
-plt.suptitle("kD values of the selected mAb variants", fontsize=16)
-# subtitle
-b.set_title("p-value = {}".format(np.round(t[1], 5)), fontsize = 14)
-# Remove axis spines
-sns.despine(offset = 5, trim = True)
-#plt.show()
-plt.savefig(os.path.join(save_path, 'KD_originally_novel_variants_Ttest.pdf'))
-
-
+###### BOX PLOT - NOVEL SEQUENCES ######
+box_plot_KDs(KD_seqs_novel, x = None, y = "KD_nM", groups=None,
+             color_palette = ['#cd0000'], fig_size = (5.5,8), save_fig = os.path.join(save_path, 'KD_novel_variants.pdf'))
 
